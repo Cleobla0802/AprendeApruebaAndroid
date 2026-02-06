60147b1d71230de1d3c315ec15ffa6c0b49f2c8b
Cristian <cleobla0802@iescarrillo.es>
2026-02-06 12:24:57 +0100

    Hotfix texto de RegisterActivity.java con login

--- app/src/main/java/es/iescarrillo/aprendeaprueba/activities/RegisterActivity.java
+++ app/src/main/java/es/iescarrillo/aprendeaprueba/activities/RegisterActivity.java
@@ -1,6 +1,8 @@
 package es.iescarrillo.aprendeaprueba.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +10,18 @@ import es.iescarrillo.aprendeaprueba.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    TextView tvLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        tvLogin = findViewById(R.id.tvLogin);
+
+        tvLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
